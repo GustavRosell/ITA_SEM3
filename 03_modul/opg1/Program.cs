eb4b165431844531507b19a043fa6c9ffaad25f5
rosell <Gustavrosell@gmail.com>
2024-02-07 10:38:49 +0100

    modul 3; opg1 update

--- 03_modul/opg1/Program.cs
+++ 03_modul/opg1/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Herunder er tre offentlige API’er hvor I skal dykke ned i dokumentationen. Læs spørgsmålene, besvar dem sammen med din gruppe, og skriv svarene ned.
+
+// Opgave 1.1: 
+
+En ordbog
+URI: https://dictionaryapi.dev/Links to an external site.
+
+Spørgsmål:
+
+1: Hvad kan man med denne API og hvad er det primære formål?
+- API’en giver adgang til en ordbog
+2: Kan API’en (mis)bruges til andre ting?
+- Automatiseret indhentning af data til kommercielle formål uden tilladelse, hvilket kan krænke servicevilkårene.
+- Overbelastning af servere med anmodninger, hvilket kan føre til denial-of-service (DoS) angreb, der forstyrrer tjenesten for andre brugere.
+- Uautoriseret indlejring af API-data i egne produkter eller tjenester på en måde, der giver indtryk af, at indholdet er ens eget.
+3: Hvor godt opfylder API’en følgende kriterier? Operational, expressive, simple og predictable.
+- API’en er operational, expressive, simple og predictable, da den er let at bruge og forstå, og den er forudsigelig i sin adfærd.
+4: Afprøv API’en hvis du ikke allerede har gjort det (browser, postman eller curl).
+- Done
+
+// Opgave 1.2: 
+
+En filmdatabase
+URI: https://ghibliapi.vercel.app/#Links to an external site.
+
+Spørgsmål:
+
+1: Hvad kan man med denne API og hvad er det primære formål?
+- API’en giver adgang til en filmdatabase
+2: Hvilke steps skal man udføre for at svare på følgende spørgsmål?
+3: Hvilke film har “cat” som en “species” der indgår i filmen?
+
+4: Hvilke “species” findes der i filmen “Totoro”?
+5: Prøv dine steps af fra punkt 2.
+6: Hvor godt opfylder API’en følgende kriterier? Operational, expressive, simple og predictable.
+
+
+// Opgave 1.3:
+
+Reddit
+URI: https://www.reddit.com/dev/api/Links to an external site.
+Reddit API’en kræver en key, så den er ikke så nem at komme i gang med, som de andre. Men kig i dokumentationen for at svare på spørgsmålene.
+
+Spørgsmål:
+
+1: Hvordan sletter man et comment eller et link?
+2: Hvordan henter man alle kommentarer fra en bestemt tråd?
+3: Hvordan opdaterer man stylesheet på et subreddit?
+4: Hvordan ændrer man et “multi” subreddit’s markdown-beskrivelse?
+5: Hvor godt opfylder API’en følgende kriterier? Operational, expressive, simple og predictable.
+
+*/
