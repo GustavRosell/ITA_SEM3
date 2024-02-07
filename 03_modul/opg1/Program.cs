5fdb211f10d778f06ad48be92f9c8dcee97c22aa
Rosell22V <Gustav@klitholm.com>
2024-02-07 11:17:29 +0100

    modul3; opg1

--- 03_modul/opg1/Program.cs
+++ 03_modul/opg1/Program.cs
@@ -30,10 +30,16 @@ Spørgsmål:
 - API’en giver adgang til en filmdatabase
 2: Hvilke steps skal man udføre for at svare på følgende spørgsmål?
 3: Hvilke film har “cat” som en “species” der indgår i filmen?
+3.1: - brug endpoint for arter 'https://ghibliapi.vercel.app/species' - Dette vil give dig information om hver art, inklusive navnet og en URL til yderligere detaljer for hver art.
+3.2: - Find 'cat' arten: Gennemgå listen over arter for at finde arten, der repræsenterer "cats". 
+I detaljerne for hver art vil der være information om, hvilke film arten optræder i, oftest angivet som en liste af URLs, der peger på specifikke film.
+3.3 - Hent film for 'cat' arten: Når du har identificeret URL'erne for de film, hvor 'cat' arten optræder, kan du bruge filmens endpoint (/films) med det specifikke ID for hver film for at hente detaljerne for de pågældende film.
 
 4: Hvilke “species” findes der i filmen “Totoro”?
+- Human, Cat, Totoro
 5: Prøv dine steps af fra punkt 2.
 6: Hvor godt opfylder API’en følgende kriterier? Operational, expressive, simple og predictable.
+- API’en er operational, expressive, simple og predictable, da den er let at bruge og forstå, og den er forudsigelig i sin adfærd --> hvis man forstår API'ens struktur og endpoints.
 
 
 // Opgave 1.3:
@@ -44,10 +50,14 @@ Reddit API’en kræver en key, så den er ikke så nem at komme i gang med, som
 
 Spørgsmål:
 
-1: Hvordan sletter man et comment eller et link?
+1: Hvordan sletter man et comment eller et link?'
+- DELETE /api/comment
 2: Hvordan henter man alle kommentarer fra en bestemt tråd?
+- GET /r/{subreddit}/comments/{article}
 3: Hvordan opdaterer man stylesheet på et subreddit?
+- POST /api/subreddit_stylesheet
 4: Hvordan ændrer man et “multi” subreddit’s markdown-beskrivelse?
+- PUT /api/multi/{multipath}/r/{srname}/description
 5: Hvor godt opfylder API’en følgende kriterier? Operational, expressive, simple og predictable.
-
+- bro det koks 
 */
