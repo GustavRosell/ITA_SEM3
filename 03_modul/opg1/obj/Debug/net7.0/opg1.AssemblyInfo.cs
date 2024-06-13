9410f4cb7ac288533f8ef7feb0d9c369aa307681
Rosell22V <Gustav@klitholm.com>
2024-06-13 10:19:12 +0200

    wtf

--- 03_modul/opg1/obj/Debug/net7.0/opg1.AssemblyInfo.cs
+++ 03_modul/opg1/obj/Debug/net7.0/opg1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("opg1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a980cd6d964db636eb4e2cc4bef7d1bd341c6ac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e70756ff0e55f3a3a86dec756d93a9e177cc87")]
 [assembly: System.Reflection.AssemblyProductAttribute("opg1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("opg1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
